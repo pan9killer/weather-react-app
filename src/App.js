e4f9ee71ba38bc3191a8b7c660535baa051a881e
pan9killer <pan9killer@gmail.com>
2021-11-08 13:46:13 +0400

    fixed start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import Find from './components/Find';
+require('dotenv').config()
 
 function App() {
   return (
     <div className="App">
+      <Find />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { store } from './app/store';
+import { store } from './store/store';
 import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <React.StrictMode>
     <Provider store={store}>
       <App />
-    </Provider>
-  </React.StrictMode>,
+    </Provider>,
   document.getElementById('root')
 );
 
