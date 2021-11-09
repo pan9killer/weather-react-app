069e29f33eb0db278b97ebb0e7ef36c42333450b
pan9killer <pan9killer@gmail.com>
2021-11-09 13:22:22 +0400

    get weather functions

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,20 +1,56 @@
 const axios = require('axios');
 const instance = axios.create({baseURL: 'https://api.openweathermap.org/data/2.5/'});
-require('dotenv').config();
 
 
-export default async function getWeatherData(){
+export async function getWeatherDataOnLoad(){
   try{
-    const getWeather = await instance.get(`onecall`, {
+    const getWeatherDataOnLoad = await instance.get(`onecall`, {
       params: {
-        // lat: ,
-        // lon: ,
-        exclude: daily,
-        appid: process.env.API_KEY
+        lat: 41.7202176,
+        lon: 44.761088,
+        exclude: 'daily',
+        lang: 'ru',
+        units: 'metric',
+        appid: '15e2d862129fa12a6ba6633847af27ed'
       }
     })
-    .then((response) => response.data);
-    return getWeather;
+    .then((response) => response);
+    return getWeatherDataOnLoad;
+  }catch(err){
+    console.log(err);
+  }
+}
+
+export async function getWeatherDataFromInput(){
+  try{
+    
+    const getWeatherDataFromInput = await instance.get(`onecall`, {
+      params: {
+        lat: -0.1257,
+        lon: 51.5085,
+        exclude: 'daily',
+        lang: 'ru',
+        units: 'metric',
+        appid: '15e2d862129fa12a6ba6633847af27ed'
+      }
+    })
+    .then((response) => response);
+    return getWeatherDataFromInput;
+  }catch(err){
+    console.log(err);
+  }
+}
+
+export async function getCityLatLon(city){
+  try{
+    const getCityLanLot = await instance.get(`weather`, {
+      params: {
+        q: city,
+        appid: '15e2d862129fa12a6ba6633847af27ed'
+      }
+    })
+    .then((response) => response.data.coord);
+    return getCityLanLot;
   }catch(err){
     console.log(err);
   }
