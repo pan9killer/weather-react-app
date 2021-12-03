28b39c2ac7b13e3a84d0092732ce2aa3f10424ba
pan9killer <pan9killer@gmail.com>
2021-12-03 13:15:27 +0400

    creating fetch from searchBlock

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,11 +1,4 @@
-export const getWeatherOnYourLocation = (payload) => ({type: 'MY_LOCATION_WEATHER', payload});
-export const getLanLotFromCity = (payload) => ({type: 'CITY_LAT_LOT', payload});
-export const getWeatherData = (payload) => ({type: 'GET_WEATHER_IN_CITY', payload});
-
-
-
-
-// export const getLatLonFromCity = () => {return async(dispatch, getState) => {
-//   const location = await getCityLatLon()
-//     dispatch({type: 'CITY_LAT_LOT', location})
-// }}
+import {MY_LOCATION_WEATHER, CITY_LAT_LOT, GET_WEATHER_IN_CITY} from './constants'
+export const getWeatherOnYourLocation = (payload) => ({type: MY_LOCATION_WEATHER, payload});
+export const getLanLotFromCity = (payload) => ({type: CITY_LAT_LOT, payload});
+export const getWeatherData = (payload) => ({type: GET_WEATHER_IN_CITY, payload});
--- src/components/searchBlock/SearchBlock.jsx
+++ src/components/searchBlock/SearchBlock.jsx
@@ -1,22 +1,45 @@
-import styles from "./SearchBlock.module.scss"
+import styles from "./SearchBlock.module.scss";
+import { useDispatch, useSelector } from 'react-redux';
+import { getCityLatLon, getWeatherDataAPI } from '../../services/api.service';
+import { useRef } from 'react';
+import { getWeatherData } from "../../actions/actions";
 
-const SearchBlock = (city) => {
+
+const SearchBlock = () => {
+  const inputEl = useRef('');
+  const dispatch = useDispatch();
+  const state = useSelector(state => state);
+  console.log(state);
+
+  const findCity = async(e) => {
+    e.preventDefault();
+    const { value } = inputEl.current;
+    const data = await getCityLatLon(value);
+    if(!data){
+      return alert('City not found! Try again.')
+    }
+    const newData = {
+      latitude: data.lat,
+      longitude: data.lon
+    }
+    const showCityWeather = await getWeatherDataAPI(newData)
+    dispatch(getWeatherData(showCityWeather))
+  };
+
   return(
     <div className={styles.search__block}>
       <h1>Weather in</h1>
-        <div className="input-group mb-3">
-        <input 
-        type="text" 
-        className="form-control" 
-        placeholder="City" 
-        aria-label="Recipient's username" 
-        aria-describedby="button-addon2" />
-        <button className="btn btn-outline-secondary" 
-        type="button" 
-        id="button-addon2"
-        // onClick={}
-        >&#128269;</button>
-      </div>
+        <form className="input-group mb-3" onSubmit={findCity} >
+          <input 
+            ref={inputEl}
+            type="text" 
+            className="form-control" 
+            placeholder="City" 
+            aria-label="Recipient's username" 
+            aria-describedby="button-addon2" />
+          <button className="btn btn-outline-secondary" id="button-addon2"
+          >&#128269;</button>
+      </form>
     </div>
   )
 }
